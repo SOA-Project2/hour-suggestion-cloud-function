d85abfc4164da1f89360ac97aa3cf4eb44ae24ad
valesskablanco <valesskablanco@estudiantec.cr>
2024-04-08 22:18:28 -0600

    schedule recommendation working

---
+++ src/Server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const PORT = 3000;
+
+const app = express();
+const router = express.Router();
+
+//const errorHandler = require("./errors/ErrorHandler");
+//router.use(errorHandler.queryValidatorMiddleware);
+
+const scheduleRecommendation = require("./business-logic/ScheduleRecommendation");
+router.get("/schedule/:day/:time", scheduleRecommendation.getRecommendation);
+
+app.use(router);
+
+app.listen(PORT, () => {
+    console.log(`Server is now listening on port ${PORT}`);
+});
+
+module.exports = {
+    app
+};
